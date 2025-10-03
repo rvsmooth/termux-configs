0110dbc47af93c96ecc03ec7201fdf4f26d00f58
rvsmooth <riveeks.smooth@gmail.com>
2025-10-03 10:45:25 +0530

    chore: add alias to update the dotfiles

--- scripts-common/aliases.sh
+++ scripts-common/aliases.sh
@@ -16,3 +16,6 @@ alias yta-best="yt-dlp --extract-audio --audio-format best "
 alias yta-flac='yt-dlp --extract-audio --audio-format flac --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s" --restrict-filenames '
 alias yta-mp3="yt-dlp --extract-audio --audio-format mp3 "
 alias ytv-best="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 "
+
+# termux
+alias cupdate="cd $HOME/.config && git pull"
