32d7d898bffa7889bba4c5af1e5a17b0e802aabf
rvsmooth <riveeks.smooth@gmail.com>
2026-02-14 11:09:45 +0530

    use && for message

--- scripts-common/storage_setup.sh
+++ scripts-common/storage_setup.sh
@@ -4,4 +4,5 @@
   echo "Storage permission is not setup" &&
   yes | termux-setup-storage &&
   sleep 10 &&
-  mkdir $HOME/storage/downloads/termux-ytdl || {echo -e "Storage is not setup properly\nexit and open termux"}
+  mkdir $HOME/storage/downloads/termux-ytdl &&
+  echo -e "Storage is not setup properly\nexit and open termux"
