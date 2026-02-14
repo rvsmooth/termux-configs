ae86ad28340f68c468ccf48e437e9550639f202b
rvsmooth <riveeks.smooth@gmail.com>
2026-02-14 10:25:55 +0530

    Revert "ytdlp.sh: convert aliases to functions"
    This reverts commit eb92bc58e73589068d2bc7fae3456c278021d6dc.

--- scripts-common/ytdlp.sh
+++ scripts-common/ytdlp.sh
@@ -18,21 +18,19 @@
 # Output Example: ./Title - Creator (Year)/Title-Year.ext
 set DESTINATION "~/storage/downloads/termux-ytdl"
 
-yt-dlp-archive() {
-  cd $DESTINATION && yt-dlp \
-    --embed-thumbnail \
-    --embed-metadata \
-    --embed-chapters \
-    --write-thumbnail \
-    --write-description \
-    --write-url-link \
-    --write-subs \
-    --write-auto-subs \
-    --sub-format srt \
-    --trim-filenames 248 \
-    --sponsorblock-mark all \
-    --output "%(title)s - %(channel,uploader)s (%(release_year,upload_date>%Y)s)/%(title)s - %(release_year,upload_date>%Y)s - [%(id)s].%(ext)s"
-}
+alias yt-dlp-archive='cd $DESTINATION && yt-dlp \
+--embed-thumbnail \
+--embed-metadata \
+--embed-chapters \
+--write-thumbnail \
+--write-description \
+--write-url-link \
+--write-subs \
+--write-auto-subs \
+--sub-format srt \
+--trim-filenames 248 \
+--sponsorblock-mark all \
+--output "%(title)s - %(channel,uploader)s (%(release_year,upload_date>%Y)s)/%(title)s - %(release_year,upload_date>%Y)s - [%(id)s].%(ext)s"'
 
 # Archiver in Playlist mode.
 # Writes files: description, thumbnail, URL link, subtitles, auto-subtitles
@@ -41,73 +39,65 @@ yt-dlp-archive() {
 # This is different from the above, to avoid large amount of folders.
 # The assumption is you want only the playlist as it appears online.
 # Output Example: ./Playlist-name/Title - Creator (Year)/Title-Year.ext
-yt-dlp-archive-playlist() {
-  cd $DESTINATION && yt-dlp \
-    --embed-thumbnail \
-    --embed-metadata \
-    --embed-chapters \
-    --write-thumbnail \
-    --write-description \
-    --write-url-link \
-    --write-subs \
-    --write-auto-subs \
-    --sub-format srt \
-    --trim-filenames 248 \
-    --sponsorblock-mark all \
-    --output "%(playlist)s/%(title)s - %(creators,creator,channel,uploader)s - %(release_year,upload_date>%Y)s - [%(id)s].%(ext)s"
-}
+alias yt-dlp-archive-playlist='cd $DESTINATION && yt-dlp \
+--embed-thumbnail \
+--embed-metadata \
+--embed-chapters \
+--write-thumbnail \
+--write-description \
+--write-url-link \
+--write-subs \
+--write-auto-subs \
+--sub-format srt \
+--trim-filenames 248 \
+--sponsorblock-mark all \
+--output "%(playlist)s/%(title)s - %(creators,creator,channel,uploader)s - %(release_year,upload_date>%Y)s - [%(id)s].%(ext)s"'
 
 # Audio Extractor
 # Writes: <ARTIST> / <ALBUM> / <TRACK> with fallback values
 # Embeds available metadata
-yt-dlp-audio-only() {
-  cd $DESTINATION && yt-dlp \
-    --embed-thumbnail \
-    --embed-metadata \
-    --embed-chapters \
-    --extract-audio \
-    --audio-quality 320K \
-    --trim-filenames 248 \
-    --output "%(artist,channel,album_artist,uploader)s/%(album)s/%(track,title,track_id)s - [%(id)s].%(ext)s"
-}
+alias yt-dlp-audio-only='cd $DESTINATION && yt-dlp \
+--embed-thumbnail \
+--embed-metadata \
+--embed-chapters \
+--extract-audio \
+--audio-quality 320K \
+--trim-filenames 248 \
+--output "%(artist,channel,album_artist,uploader)s/%(album)s/%(track,title,track_id)s - [%(id)s].%(ext)s"'
 
 # Batch mode for downloading multiple videos from a list of URLs in a file.
 # Must provide a file containing URL's as your argument.
 # Writes files: description, thumbnail, URL link, subtitles, auto-subtitles
 #
 # Example usage: yt-dlp-batch ~/urls.txt
-yt-dlp-batch() {
-  cd $DESTINATION && yt-dlp \
-    --embed-thumbnail \
-    --embed-metadata \
-    --embed-chapters \
-    --write-thumbnail \
-    --write-description \
-    --write-url-link \
-    --write-subs \
-    --write-auto-subs \
-    --sub-format srt \
-    --trim-filenames 248 \
-    --sponsorblock-mark all \
-    --output "%(title)s - %(channel,uploader)s (%(release_year,upload_date>%Y)s)/%(title)s - %(release_year,upload_date>%Y)s - [%(id)s].%(ext)s" \
-    --batch-file
-}
+alias yt-dlp-batch='cd $DESTINATION && yt-dlp \
+--embed-thumbnail \
+--embed-metadata \
+--embed-chapters \
+--write-thumbnail \
+--write-description \
+--write-url-link \
+--write-subs \
+--write-auto-subs \
+--sub-format srt \
+--trim-filenames 248 \
+--sponsorblock-mark all \
+--output "%(title)s - %(channel,uploader)s (%(release_year,upload_date>%Y)s)/%(title)s - %(release_year,upload_date>%Y)s - [%(id)s].%(ext)s" \
+--batch-file'
 
 # Livestream recording.
 # Writes files: thumbnail, url link, subs and auto-subs (if available).
 # Also writes files: Info.json and Live Chat if available.
-yt-dlp-livestream() {
-  cd $DESTINATION && yt-dlp \
-    --live-from-start \
-    --write-thumbnail \
-    --write-url-link \
-    --write-subs \
-    --write-auto-subs \
-    --write-info-json \
-    --sub-format srt \
-    --trim-filenames 248 \
-    --output "%(title)s - %(channel,uploader)s (%(upload_date)s)/%(title)s - (%(upload_date)s) - [%(id)s].%(ext)s"
-}
+alias yt-dlp-livestream='cd $DESTINATION && yt-dlp \
+--live-from-start \
+--write-thumbnail \
+--write-url-link \
+--write-subs \
+--write-auto-subs \
+--write-info-json \
+--sub-format srt \
+--trim-filenames 248 \
+--output "%(title)s - %(channel,uploader)s (%(upload_date)s)/%(title)s - (%(upload_date)s) - [%(id)s].%(ext)s"'
 
 ##############
 # UTILITIES:
@@ -117,18 +107,16 @@ yt-dlp-livestream() {
 # Only download metadata, no downloading of video or audio files
 # Writes files: Description, Info.json, Thumbnail, URL Link, Subtitles
 # The usecase for this tool is grabbing extras for videos you already have downloaded, or to only grab metadata about a video.
-yt-dlp-get-metadata() {
-  cd $DESTINATION && yt-dlp \
-    --skip-download \
-    --write-description \
-    --write-info-json \
-    --write-thumbnail \
-    --write-url-link \
-    --write-subs \
-    --write-auto-subs \
-    --sub-format srt \
-    --trim-filenames 248
-}
+alias yt-dlp-get-metadata='cd $DESTINATION && yt-dlp \
+--skip-download \
+--write-description \
+--write-info-json \
+--write-thumbnail \
+--write-url-link \
+--write-subs \
+--write-auto-subs \
+--sub-format srt \
+--trim-filenames 248'
 
 # Takes in a playlist URL, and generates a CSV of the data.
 # Writes a CSV using a pipe { | } as a delimiter, allowing common delimiters in titles.
@@ -142,14 +130,12 @@ yt-dlp-get-metadata() {
 # Documents columns:
 # Webpage URL, Playlist Index Number, Title, Channel/Uploader, Creators,
 # Channel/Uploader URL, Release Year, Duration, Video Availability, Description, Tags
-yt-dlp-export-playlist-info() {
-  cd $DESTINATION && yt-dlp \
-    --skip-download \
-    --cookies-from-browser firefox \
-    --ignore-errors \
-    --ignore-no-formats-error \
-    --flat-playlist \
-    --trim-filenames 248 \
-    --print-to-file "%(webpage_url)s#|%(playlist_index)05d|%(title)s|%(channel,uploader,creator)s|%(creators)s|%(channel_url,uploader_url)s|%(release_year,upload_date)s|%(duration>%H:%M:%S)s|%(availability)s|%(description)s|%(tags)s" "%(playlist_title,playlist_id)s.csv" \
-    --replace-in-metadata title "[\|]+" "-"
-}
+alias yt-dlp-export-playlist-info='cd $DESTINATION && yt-dlp \
+--skip-download \
+--cookies-from-browser firefox \
+--ignore-errors \
+--ignore-no-formats-error \
+--flat-playlist \
+--trim-filenames 248 \
+--print-to-file "%(webpage_url)s#|%(playlist_index)05d|%(title)s|%(channel,uploader,creator)s|%(creators)s|%(channel_url,uploader_url)s|%(release_year,upload_date)s|%(duration>%H:%M:%S)s|%(availability)s|%(description)s|%(tags)s" "%(playlist_title,playlist_id)s.csv" \
+--replace-in-metadata title "[\|]+" "-"'
